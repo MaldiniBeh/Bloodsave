5657ad28c64d1431e0527677ed0b7880f83796b8
MaldiniBeh <69940992+MaldiniBeh@users.noreply.github.com>
2024-07-31 00:12:13 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: 'AIzaSyCseaXpg0R5B_Q0cN6L6_uwgSMhcXzr3yU',
-    authDomain: 'bloodsave-49dfa.firebaseapp.com',
-    projectId: 'bloodsave-49dfa',
-    storageBucket: 'bloodsave-49dfa.appspot.com',
-    messagingSenderId: '985878527882',
-    appId: '1:985878527882:web:9642a2310709ad3e4bd273',
-    measurementId: 'G-JY7SZZ3803'
+    apiKey: '**************',
+    authDomain: '********',
+    projectId: '*******',
+    storageBucket: '*******',
+    messagingSenderId: '*********',
+    appId: '*********',
+    measurementId: '*******'
   },
-  Tomtom: 'WiwOABGApJFLCHWFq0dh3Zcm5rM51afo'
+  Tomtom: '*************'
 };
